14d8696375bc6d58e0a6f21d24b13351deadb170
fish <mikefischthal@gmail.com>
2023-08-12 13:52:27 -0400

    workin

--- schedule.js
+++ schedule.js
@@ -17,13 +17,44 @@ const schedule = {
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-    setColors(schedule.NAVY.dayA);
+    getToday();
 });
   
 function setColors(day) {
+    console.log(day);
     document.getElementById("p0").style.backgroundColor = day[0];
     document.getElementById("p1").style.backgroundColor = day[1];
     document.getElementById("p2").style.backgroundColor = day[2];
     document.getElementById("p3").style.backgroundColor = day[3];
     document.getElementById("p4").style.backgroundColor = day[4];
+
+    document.getElementById("p0").querySelector('.tooltip').textContent = day[0];
+    document.getElementById("p1").querySelector('.tooltip').textContent = day[1];
+    document.getElementById("p2").querySelector('.tooltip').textContent = day[2];
+    document.getElementById("p3").querySelector('.tooltip').textContent = day[3];
+    document.getElementById("p4").querySelector('.tooltip').textContent = day[4];
 }
+
+function getToday() {
+    const currentDate = new Date().toISOString().split('T')[0];
+    fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vS3-6MgEPFUcHbLfa7q97_I6BI8CJvLZA0FDPxMwKOEFKYZs1GAw_4CRt6oOIWhMEITpOKzYrW2u7Ef/pub?gid=0&single=true&output=csv')
+        .then(response => response.text())
+        .then(data => {
+        // Splitting the CSV data into lines and parsing
+        const lines = data.split('\n');
+        for (let i = 1; i < lines.length; i++) {
+        const [date, scheduleDay] = lines[i].split(',');
+        if (date === currentDate) {
+            correctDay = scheduleDay.trim()
+            console.log(correctDay)
+            colors = schedule.NAVY[correctDay];
+            setColors(colors);
+            return;
+        }
+        }
+        console.log(`No schedule found for ${currentDate}`);
+    })
+    .catch(error => console.error('An error occurred:', error));
+
+}
+
